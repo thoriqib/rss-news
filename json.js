878316ba11b31013450a527131e1e3bb85101c54
Thoriq Ibadurrohman <thoriq312@gmail.com>
2020-09-11 08:23:22 +0700

    Menambah 2 rss lagi

--- json.js
+++ json.js
@@ -1,33 +1,44 @@
-const berita = document.querySelector(".berita");
+const URL = [
+  `https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Frss.detik.com%2Findex.php%2Fdetikcom_nasional`,
+  `https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Frss.detik.com%2Findex.php%2Ffinance`,
+  `https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.vice.com%2Fid_id%2Frss`,
+];
 
-fetch(
-  `https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Frss.detik.com%2Findex.php%2Fdetikcom_nasional`
-)
-  .then((response) => {
-    return response.json();
-  })
-  .then((responseJson) => {
-    if (responseJson.error) {
-      showResponseMessage(responseJson.message);
-    } else {
-      items = responseJson.items;
-      console.log(responseJson);
-      let content = ``;
-      for (let i = 0; i < items.length; i++) {
-        content += `
-            <div class="card m-3 " style="width: 18rem;">
-            <img src="${items[i].enclosure.link}" class="card-img-top" alt="Berita">
-            <div class="card-body">
-                <h5 class="card-title">${items[i].title}</h5>
-                <p class="card-text">${items[i].description}</p>
-                <a href="${items[i].link}" class="btn btn-primary">Detail berita</a>
-            </div>
-            </div>
-        `;
+const fetchData = (link) => {
+  fetch(link)
+    .then((response) => {
+      return response.json();
+    })
+    .then((responseJson) => {
+      if (responseJson.error) {
+        showResponseMessage(responseJson.message);
+      } else {
+        items = responseJson.items;
+        document.querySelector(".berita").innerHTML += showData(items);
       }
-      berita.innerHTML = content;
-    }
-  })
-  .catch((error) => {
-    showResponseMessage(error);
-  });
+    })
+    .catch((error) => {
+      showResponseMessage(error);
+    });
+};
+
+const showData = (items) => {
+  let content = ``;
+  for (let i = 0; i < items.length; i++) {
+    content += /*html*/ `
+              <div class="card m-3 " style="width: 18rem;">
+                <img src="${items[i].enclosure.link}" class="card-img-top" alt="Berita">
+                <div class="card-body">
+                    <h5 class="card-title">${items[i].title}</h5>
+                    <p class="card-text">${items[i].description}</p>
+                    <a href="${items[i].link}" class="btn btn-primary">Detail berita</a>
+                </div>
+              </div>
+          `;
+  }
+  return content;
+};
+
+URL.forEach((link) => {
+  fetchData(link);
+});
--- xml.js
+++ xml.js
@@ -1,29 +1,40 @@
-const RSS_URL = `http://rss.detik.com/index.php/detikcom_nasional`;
+const URL = [
+  `http://rss.detik.com/index.php/detikcom_nasional`,
+  `https://rss.detik.com/index.php/finance`,
+  `https://www.vice.com/id_id/rss`,
+];
 
-fetch(RSS_URL)
-  .then((response) => response.text())
-  .then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
-  .then((data) => {
-    console.log(data);
-    const items = data.querySelectorAll("item");
-    console.log(items);
-    let html = ``;
-    items.forEach((el) => {
-      let title = el.querySelector("title").textContent;
-      let link = el.querySelector("link").textContent;
-      let description = el.querySelector("description").textContent;
-      let date = el.querySelector("pubDate").textContent;
-      html += `
-        <div class="card m-3" style="width: 18rem;">
-        <div class="card-body">
-          <h5 class="card-title">${title}</h5>
-          <h6 class="card-subtitle mb-2 text-muted">${date}</h6>
-          <p class="card-text">${description}</p>
-          <a href="${link}" class="card-link">Detail Berita</a>
-        </div>
-      </div>
-      `;
+const fetchData = (link) => {
+  fetch(link)
+    .then((response) => response.text())
+    .then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
+    .then((data) => {
+      const items = data.querySelectorAll("item");
+      document.querySelector(".berita").innerHTML += showData(items);
     });
-    console.log(html);
-    document.querySelector(".berita").innerHTML = html;
+};
+
+const showData = (items) => {
+  let html = ``;
+  items.forEach((el) => {
+    let title = el.querySelector("title").textContent;
+    let link = el.querySelector("link").textContent;
+    let description = el.querySelector("description").textContent;
+    let date = el.querySelector("pubDate").textContent;
+    html += /*html*/ `
+            <div class="card m-3" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">${title}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">${date}</h6>
+              <p class="card-text">${description}</p>
+              <a href="${link}" class="card-link">Detail Berita</a>
+            </div>
+          </div>
+          `;
   });
+  return html;
+};
+
+URL.forEach(link => {
+  fetchData(link);
+});
